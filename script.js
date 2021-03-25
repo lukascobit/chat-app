56087d4812bd729bb123a040237b3c0bbe8ba541
Lukas <77848587+lukascobit@users.noreply.github.com>
2021-03-25 17:47:13 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+const chatForm = document.getElementById('chat-form');
+const chatMsg = document.querySelector('.chat-messages');
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users');
+
+// get username and room from URL
+const { username, room} = Qs.parse(location.search, {
+  ignoreQueryPrefix: true
+})
+
+
+console.log(room ,username);
+
+
+const socket = io();
+
+//join chatroom
+socket.emit('joinRoom' , {username, room});
+
+//get room and users
+socket.on('roomUsers', ({ room, users}) => {
+  outputRoomName(room);
+  outputUsers(users);
+})
+
+
+
+//server message
+socket.on('message', message => {
+  console.log(message);
+  outputMessage(message);
+
+  //scrolldown
+  chatMsg.scrollTop = chatMsg.scrollHeight;
+});
+
+//message submit 
+chatForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  //getting the message text
+  const msg = e.target.elements.msg.value;
+
+  // emitting message to the server
+  socket.emit('chatMsg' , msg);
+
+  //clear input
+  e.target.elements.msg.value = '';
+  e.target.elements.msg.focus()
+});
+
+
+
+//output the message
+function outputMessage(message){
+  const div = document.createElement('div');
+  div.classList.add('message');
+  div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p>
+  <p class="text">
+    ${message.text}
+  </p>`;
+  document.querySelector('.chat-messages').appendChild(div);
+}
+
+//add name
+function outputRoomName(room){
+  roomName.innerText = room;
+}
+
+
+//show users
+function outputUsers(users){
+  userList.innerHTML = `
+    ${users.map(user => `<li>${user.username}</li>`).join('')}
+  `;
+
+}
